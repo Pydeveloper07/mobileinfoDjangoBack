71b4d405cba08c6696ae7484326f23743661ccac
Tukhtamurod Isroilov <inha07111999@gmail.com>
2020-05-20 20:02:12 +0500

    logout and registration finished

--- accounts/views.py
+++ accounts/views.py
@@ -25,8 +25,47 @@ def login(request):
     else:
         return render(request, 'pages/index.html')
 
+@login_required
 def logout(request):
-    pass
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, "You are now logged out!")
+        return redirect('index')
+        
 
 def register(request):
-    pass
+    if request.method == "POST":
+        first_name = last_name = username = email = password = conf_password = ''
+        if request.POST['first_name']:
+            first_name = request.POST['first_name']
+        if request.POST['last_name']:
+            last_name = request.POST['last_name']
+        if request.POST['username']:
+            username = request.POST['username']
+        else:
+            messages.error(request, "Username required!")
+            return redirect('index')
+        if request.POST['email']:
+            email = request.POST['email']
+        if request.POST['password']:
+            password = request.POST['password']
+        if request.POST['conf_password']:
+            conf_password = request.POST['conf_password']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "This username already exists! Try another!")
+            return redirect('index')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "This email already in use!")
+            return redirect('index')
+        else:
+            if password == conf_password and len(password) >= 6:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+                user.save()
+                messages.success(request, "You have successfully registered!")
+                return redirect('index')
+            else:
+                messages.error(request, "Password length shouldn't be less than 6 characters!")
+                return redirect('index')
+
+        
+
